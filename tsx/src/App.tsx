fa27cac18d7c1c684c21a7d639d531fbae7bf2b8
Bestin John <bestinjohn1993@gmail.com>
2025-04-26 23:09:08 +0200

    feat: implement booking summary feature in App component

--- tsx/src/App.tsx
+++ tsx/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const bookings = [
+    { id: 1, user: "Alice", hotel: "Hotel A", nights: 3 },
+    { id: 2, user: "Bob", hotel: "Hotel B", nights: 2 },
+    { id: 3, user: "Alice", hotel: "Hotel C", nights: 1 },
+  ];
 
+  const bookingsPerUser = bookings.reduce((acc, booking) => {
+    // Recommend the below in JS Env
+    // if (!acc[booking.user]) {
+    //   acc[booking.user] = { nights: 0 };
+    // }
+    // acc[booking.user].nights += booking.nights;
+
+    acc[booking.user] = {
+      nights: acc[booking.user]?.nights ?? 0 + booking.nights,
+    };
+    return acc;
+  }, {} as Record<string, { nights: number }>);
+
+  console.log("🚀 ~ bookingsPerUser ~ bookingsPerUser:", bookingsPerUser);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ul>
+        {Object.entries(bookingsPerUser).map(([user, value]) => (
+          <li key={user}>{`${user} Booked ${value.nights} night/nights`}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
